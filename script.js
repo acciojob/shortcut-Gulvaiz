9e8e9a2c55cf53d2565bc70a60d459a9bee46f29
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-07-31 14:20:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function shortcut(s1, s2) {
   // your code here
+	 if(s1 == "" || s2 == ""){
+          return ("")
+     }
+     
+     else{
+         while(i=0){
+            s3 = s1[i]
+            s4 = s2[i]
+         }
+         return (s3+s4)
+     }
 }
 
 // Do not change the code below.
